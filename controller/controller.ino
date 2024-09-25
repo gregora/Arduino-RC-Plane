c85398b2580895232ab68547e73d90f2433787fe
Gregor Alič <gregor372@gmail.com>
2024-09-25 17:03:52 +0200

    Start to connect controller to the receiver via iBUS protocol

--- controller/controller.ino
+++ controller/controller.ino
@@ -4,6 +4,9 @@
 #include <Adafruit_BNO055.h>
 #include <Servo.h>
 
+#include <IBusBM.h>
+#include <SoftwareSerial.h>
+
 Servo ch1;
 Servo ch2;
 Servo ch3;
@@ -11,20 +14,25 @@ Servo ch4;
 Servo ch5;
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+IBusBM IBus;    // IBus object
+
+SoftwareSerial receiver(6, 7);
+uint8_t buffer[32];
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  receiver.begin(115200);
 
   ch1.attach(2);
   ch2.attach(3);
   ch3.attach(4);
   ch4.attach(5);
-  ch5.attach(6);
+  //ch5.attach(6);
 
   if(!bno.begin())
   {
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+    //while(1);
   }
 
 
@@ -51,11 +59,23 @@ void loop() {
   Serial.print(roll, 4);
   Serial.println("");
 
-  
 
   ch1.write(pitch + 90);
 
+  Serial.print("\n");
+
+  int val = IBus.readChannel(2);
+  Serial.print(val);
+  Serial.print("\n");
+
+  if(receiver.available()){
+      for(int i = 0; i < 32; i++){
+        Serial.print(" ");
+        Serial.print(receiver.read());
+      }
+      Serial.print("\n");
+  }
 
   delay(50);
-
+  
 }
