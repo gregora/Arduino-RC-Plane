9532a1ee4b949922c30c9527e12f3fcc8d93e64b
Gregor Alič <gregor372@gmail.com>
2024-09-28 22:52:42 +0200

    Optimize PCB traces and update code

--- controller/controller.ino
+++ controller/controller.ino
@@ -16,38 +16,34 @@ Servo ch4;
 Servo ch5;
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
-IBusBM IBus;    // IBus object
 
-int channels[14];
+IBusBM IBus;
+int channels[14]; // buffer to store read values
 
 void setup() {
-  //Serial.begin(115200);
-  IBus.begin(Serial);
-
+  // attach the motors to corresponding pins
   ch1.attach(2);
   ch2.attach(3);
   ch3.attach(4);
   ch4.attach(5);
   ch5.attach(6);
 
-  if(!bno.begin())
-  {
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    //while(1);
-  }
+  IBus.begin(Serial); // use Serial to read flysky receiver iBUS
 
-  Wire.begin(8);                // join i2c bus with address #8
-  Wire.onRequest(requestEvent); // register event
+  Wire.begin(8); // join i2c bus with address #8
+  Wire.onRequest(dataRequest); // add interrupt
   
 }
 void loop() {
 
+  // center the motors
   ch1.write(90);
   ch2.write(90);
   ch3.write(90);
   ch4.write(90);
   ch5.write(90);
 
+  // read BNO055 attitude data
   sensors_event_t event;
   bno.getEvent(&event);
 
@@ -55,35 +51,18 @@ void loop() {
   float pitch = event.orientation.y;
   float roll = event.orientation.z;
   
-  /*
-  Serial.print(yaw, 4);
-  Serial.print(" ");
-  Serial.print(pitch, 4);
-  Serial.print(" ");
-  Serial.print(roll, 4);
-  Serial.println("");
-
-  Serial.print("\n");
-  */
-
-
-  ch1.write(pitch + 90);
-
-
+  // read flysky receiver data
   for(int i = 0; i < 14; i++){
     channels[i] = IBus.readChannel(i);
   }
 
-  //val = 17;
-  //Serial.print(val);
-  //Serial.print("\n");
 
-  delay(50);
-  
+  delay(50);  
 }
 
-void requestEvent() {
 
+void dataRequest() {
+  // send flysky receiver data
   for(int i = 0; i < 14; i++){
     Wire.write(highByte(channels[i]));
     Wire.write(lowByte(channels[i]));
