cf9f32839ea4b5a4b181f29c80b7c04e3b76b29f
Gregor Alič <gregor372@gmail.com>
2024-11-02 18:38:03 +0100

    Radio is now connected and unnecessary connections are removed from the pcb

--- controller/controller.ino
+++ controller/controller.ino
@@ -7,7 +7,8 @@
 #include <IBusBM.h>
 #include <SoftwareSerial.h>
 
-#include "Wire.h"
+
+#include <ELECHOUSE_CC1101_SRC_DRV.h>
 
 Servo ch1;
 Servo ch2;
@@ -18,7 +19,19 @@ Servo ch5;
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 IBusBM IBus;
-int channels[14]; // buffer to store read values
+
+bool bno_status = false;
+bool cc1101_status = false;
+
+struct Packet{
+  float yaw;
+  float pitch;
+  float roll;
+  int channels[14];
+};
+
+Packet p;
+
 
 void setup() {
   // attach the motors to corresponding pins
@@ -27,14 +40,34 @@ void setup() {
   ch3.attach(4);
   ch4.attach(5);
   ch5.attach(6);
+  
+  //Serial.begin(115200);
 
   IBus.begin(Serial); // use Serial to read flysky receiver iBUS
 
-  Wire.begin(8); // join i2c bus with address #8
-  Wire.onRequest(dataRequest); // add interrupt
-  
+  if(!bno.begin()){
+    bno_status = false;
+    //Serial.println("BNO055: Connection ERROR");
+  }
+
+  if (ELECHOUSE_cc1101.getCC1101()){        // Check the CC1101 Spi connection.
+    Serial.println("CC1101: Connection OK");
+    cc1101_status = true;
+  }else{
+    //Serial.println("CC1101: Connection ERROR");
+  }
+
+  ELECHOUSE_cc1101.Init();              // must be set to initialize the cc1101!
+  ELECHOUSE_cc1101.setCCMode(1);       // set config for internal transmission mode.
+  ELECHOUSE_cc1101.setModulation(0);  // set modulation mode. 0 = 2-FSK, 1 = GFSK, 2 = ASK/OOK, 3 = 4-FSK, 4 = MSK.
+  ELECHOUSE_cc1101.setMHZ(433.92);   // Here you can set your basic frequency. The lib calculates the frequency automatically (default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ. Read More info from datasheet.
+  ELECHOUSE_cc1101.setSyncMode(2);  // Combined sync-word qualifier mode. 0 = No preamble/sync. 1 = 16 sync word bits detected. 2 = 16/16 sync word bits detected. 3 = 30/32 sync word bits detected. 4 = No preamble/sync, carrier-sense above threshold. 5 = 15/16 + carrier-sense above threshold. 6 = 16/16 + carrier-sense above threshold. 7 = 30/32 + carrier-sense above threshold.
+  // ELECHOUSE_cc1101.setPA(10);      // set TxPower. The following settings are possible depending on the frequency band.  (-30  -20  -15  -10  -6    0    5    7    10   11   12) Default is max!
+  ELECHOUSE_cc1101.setCrc(1);     // 1 = CRC calculation in TX and CRC check in RX enabled. 0 = CRC disabled for TX and RX.
+
 }
 void loop() {
+
 
   // center the motors
   ch1.write(90);
@@ -44,27 +77,24 @@ void loop() {
   ch5.write(90);
 
   // read BNO055 attitude data
-  sensors_event_t event;
-  bno.getEvent(&event);
+  if(bno_status){
+    sensors_event_t event;
+    bno.getEvent(&event);
 
-  float yaw = event.orientation.x;
-  float pitch = event.orientation.y;
-  float roll = event.orientation.z;
-  
+    p.yaw = event.orientation.x;
+    p.pitch = event.orientation.y;
+    p.roll = event.orientation.z;
+  }
+
   // read flysky receiver data
   for(int i = 0; i < 14; i++){
-    channels[i] = IBus.readChannel(i);
+    p.channels[i] = IBus.readChannel(i);
   }
 
+  // send data over radio
+  if(cc1101_status){
+    ELECHOUSE_cc1101.SendData((void*) &p, sizeof(p), 100);
+  }
 
   delay(50);  
-}
-
-
-void dataRequest() {
-  // send flysky receiver data
-  for(int i = 0; i < 14; i++){
-    Wire.write(highByte(channels[i]));
-    Wire.write(lowByte(channels[i]));
-  }
 }
--- ground/ground.ino
+++ ground/ground.ino
@@ -1,30 +1,68 @@
 #include <Wire.h>
+#include <ELECHOUSE_CC1101_SRC_DRV.h>
 
-int channels[14];
+struct Packet{
+  float yaw;
+  float pitch;
+  float roll;
+  int channels[14];
+};
+
+Packet p;
+
+void printPacket(Packet p){
+  Serial.print("Yaw: ");
+  Serial.println(p.yaw);
+
+  Serial.print("Pitch: ");
+  Serial.println(p.pitch);
+
+  Serial.print("Roll: ");
+  Serial.println(p.roll);
+
+  Serial.print("Channels: \n");
+  for(int i = 0; i < 14; i++){
+    Serial.print(p.channels[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
 
 void setup() {
 
-  Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
+
+  if (ELECHOUSE_cc1101.getCC1101()){        // Check the CC1101 Spi connection.
+    Serial.println("CC1101: Connection OK");
+  }else{
+    Serial.println("CC1101: Connection ERROR");
+  }
+
+  ELECHOUSE_cc1101.Init();              // must be set to initialize the cc1101!
+  ELECHOUSE_cc1101.setCCMode(1);       // set config for internal transmission mode.
+  ELECHOUSE_cc1101.setModulation(0);  // set modulation mode. 0 = 2-FSK, 1 = GFSK, 2 = ASK/OOK, 3 = 4-FSK, 4 = MSK.
+
+  ELECHOUSE_cc1101.setMHZ(433.92);   // Here you can set your basic frequency. The lib calculates the frequency automatically (default = 433.92).The cc1101 can: 300-348 MHZ, 387-464MHZ and 779-928MHZ. Read More info from datasheet.
+  ELECHOUSE_cc1101.setSyncMode(2);  // Combined sync-word qualifier mode. 0 = No preamble/sync. 1 = 16 sync word bits detected. 2 = 16/16 sync word bits detected. 3 = 30/32 sync word bits detected. 4 = No preamble/sync, carrier-sense above threshold. 5 = 15/16 + carrier-sense above threshold. 6 = 16/16 + carrier-sense above threshold. 7 = 30/32 + carrier-sense above threshold.
+  ELECHOUSE_cc1101.setCrc(1);      // 1 = CRC calculation in TX and CRC check in RX enabled. 0 = CRC disabled for TX and RX.
 
 }
 
 
 void loop() {
+    //Checks whether something has been received.
+    //When something is received we give some time to receive the message in full.(time in millis)
+    if (ELECHOUSE_cc1101.CheckRxFifo(100)){
+      if (ELECHOUSE_cc1101.CheckCRC()){    //CRC Check. If "setCrc(false)" crc returns always OK!
+        //Serial.print("Rssi: ");
+        //Serial.println(ELECHOUSE_cc1101.getRssi());
+        //Serial.print("LQI: ");
+        //Serial.println(ELECHOUSE_cc1101.getLqi());
+        
+        int len = ELECHOUSE_cc1101.ReceiveData((void*) &p);
 
-  Wire.requestFrom(8, 28);
-  
-  for(int i = 0; i < 14; i++){
-    byte x1 = Wire.read();
-    byte x2 = Wire.read();
-    int val = ((int) x1 << 8) | ((int) x2);
-    channels[i] = val;
-    Serial.print(val);
-    Serial.print(" ");
-  }
-  Serial.print("\n");
+        printPacket(p);
 
-
-  delay(50);
-
+      }
+    }
 }
