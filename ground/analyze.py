fa189e1efa139f38dd49e660424dbeeae6fa982f
gregora <gregor372@gmail.com>
2025-10-01 22:46:08 +0200

    Improve analysis

--- ground/analyze.py
+++ ground/analyze.py
@@ -5,33 +5,55 @@ from matplotlib import pyplot as plt
 
 import os
 import sys
+from scipy.signal import savgol_filter
 
 # Load the data
 file = sys.argv[1]
 data = pd.read_csv(file)
 
-# Plot roll, pitch, and yaw on one graph
-plt.subplot(2, 1, 1)
+# big chart format
+plt.figure(figsize=(15, 10))
 
-#plt.plot(data["Time"], data["Roll"], label="Roll")
-#plt.plot(data["Time"], data["Pitch"], label="Pitch")
-#plt.plot(data["Time"], data["Yaw"], label="Yaw")
 
-plt.plot(data["Time"], data["ax"], label="ax")
-plt.plot(data["Time"], data["ay"], label="ay")
-plt.plot(data["Time"], data["az"], label="az")
+plt.subplot(2, 2, 1)
+
+plt.plot(data["Time"] / 1000, data["Roll"], label="Roll")
+plt.plot(data["Time"] / 1000, data["Pitch"], label="Pitch")
+plt.plot(data["Time"] / 1000, data["Yaw"], label="Yaw")
+
+plt.legend()
+
+plt.subplot(2, 2, 2)
+#smooth data
+data["ax"] = savgol_filter(data["ax"], window_length=1, polyorder=0)
+data["ay"] = savgol_filter(data["ay"], window_length=1, polyorder=0)
+data["az"] = savgol_filter(data["az"], window_length=1, polyorder=0)
+
+plt.plot(data["Time"] / 1000, data["ax"], label="ax")
+plt.plot(data["Time"] / 1000, data["ay"], label="ay")
+plt.plot(data["Time"] / 1000, data["az"], label="az")
 
 plt.legend()
 
 
-# Plot channels on another graph
-plt.subplot(2, 1, 2)
+plt.subplot(2, 2, 3)
 
 for i in range(5):
     plt.plot(data["Time"], data["Channel_" + str(i)], label="Channel_" + str(i))
 
 plt.legend()
 
+plt.subplot(2, 2, 4)
+
+northing = data["Latitude"] - data["Latitude"][0]
+easting  = data["Longitude"] - data["Longitude"][0]
+
+northing = northing * 40_075 * 1000 / 360
+easting  = easting  * 40_075 * 1000 / 360 * np.cos(data["Latitude"][0] * np.pi / 180)
+plt.scatter(easting, northing, s = 1)
+plt.xlabel("Easting [m]")
+plt.ylabel("Northing [m]")
+plt.axis("equal")
 plt.show()
 
 
